3d9e48d672f1eaf38a34ee4dc71891db8cb0d3f7
charmyyy <844806674@qq.com>
2016-08-12 14:06:34 +0800

    动态类

---
+++ DataExch/src/domain/ObjTab.java
@@ -0,0 +1,51 @@
+package domain;
+
+public class ObjTab {
+	/*
+	 * 对目标表的动态配置 利用DynamicBean进行封装
+	 */
+	private String col;// 生成表的列名
+	private String java_type;// 列名封装后对应的java类型
+	private String exp;// 对列名的解释
+	private String db_type;// 对应表的列的类型
+
+	public String getDb_type() {
+		return db_type;
+	}
+
+	public void setDb_type(String db_type) {
+		this.db_type = db_type;
+	}
+
+	public String getCol() {
+		return col;
+	}
+
+	public void setCol(String col) {
+		this.col = col;
+	}
+
+	public String getJava_type() {
+		return java_type;
+	}
+
+	public void setJava_type(String java_type) {
+		this.java_type = java_type;
+	}
+
+	public String getExp() {
+		return exp;
+	}
+
+	public void setExp(String exp) {
+		this.exp = exp;
+	}
+
+	@Override
+	public String toString() {
+		return "ObjTab [col=" + col + ", java_type=" + java_type + ", exp="
+				+ exp + ", db_type=" + db_type + "]";
+	}
+
+
+}
---
+++ DataExch/src/test/ObjDaoTest.java
@@ -0,0 +1,44 @@
+package test;
+
+import java.rmi.server.ObjID;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.junit.Test;
+
+import Utils.ObjDBCPUtils;
+import dao.ObjDao;
+import dao.impl.ObjDaoImpl;
+import domain.ObjTab;
+
+public class ObjDaoTest {
+
+	private ObjDao od = new ObjDaoImpl();
+	
+	@Test
+	public void testInsertExchData() {
+	}
+
+	@Test
+	public void testGetTableColss() {
+		List<ObjTab> list = od.getTableColss();
+		for(ObjTab ot:list){
+			System.out.println(ot);
+		}
+	}
+	@Test
+	public void testInsertCol() {
+		ObjTab ot = new ObjTab();
+		ot.setCol("vol");
+		ot.setDb_type("NUMBER");
+		ot.setExp("µÁ—π");
+		ot.setJava_type("java.lang.Float");
+		od.insertCol(ot);
+	}
+	@Test
+	public void testGetColNum() {
+		System.out.println(od.getColNum());
+	}
+
+}
